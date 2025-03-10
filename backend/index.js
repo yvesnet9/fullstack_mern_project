eb75d7c4e5e217623086c1b530d061b53f38cb11
yves <yvesnet9@gmail.com>
2025-03-10 15:55:22 +0100

    connextion à la base de données

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
 
 
 const app = express();
+
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connection reussi')
+}).catch((err)=>{
+    console.log('connexion non reussi', err)
+})
+
 
 app.listen(3000,()=>{
     console.log('le serveur est éxecuté sur le port 3000!');
