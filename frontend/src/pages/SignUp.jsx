e58d0fcb8997c34f343e4448f38eb8919db291d4
yves <yvesnet9@gmail.com>
2025-04-29 15:32:38 +0200

    Création de formulaire d'inscription des utilisateurs

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,8 +1,22 @@
-
+import {Link} from "react-router-dom"
 
 function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Inscription</h1>
+      <form className="flex-col flex gap-4">
+        <input type="text" className="border p-3 rounded-lg" placeholder="Entrez votre nom" id="Nom"/>
+        <input type="email" className="border p-3 rounded-lg" placeholder="Entrez votre email" id="Email"/>
+        <input type="password" className="border p-3 rounded-lg" placeholder="Entrez votre mot de passe" id="Mot de passe"/>
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Inscription</button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Vous avez déjà un compte?</p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700">Connexion</span>
+        </Link>
+      </div>
+    </div>
   )
 }
 
