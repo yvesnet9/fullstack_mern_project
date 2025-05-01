961d04754bcce474eee82fe37d25801ae94fd487
yves <yvesnet9@gmail.com>
2025-05-01 17:58:14 +0200

    Création de l'api pour la connexion de l'utilisateur

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/errors.js';
+import jwt from"jsonwebtoken"
 
 export const signup = async(req, res, next) => {
     const { username, email, password} = req.body;
@@ -13,3 +14,35 @@ export const signup = async(req, res, next) => {
         next(error)
     }
 }
+//  Api signin (connexion)
+
+
+    export const signin = async (req, res, next) =>{
+        // requette connexion user
+        const {email, password} = req.body
+
+        try {
+            // recup l'utilisateur connecter depuis la bdd
+            const validUser = await User.findOne({email});
+            // si l'utilisateur n'existe pas
+            if (!validUser) return next (errorHandler(404, 'Utilisateur non trouvé!'));
+            // si il existe on compare les mdp
+            const validPassword = bcryptjs.compareSync(password, validUser.password)
+            // mdp ne match pas
+            if(!validPassword) return next(errorHandler(401, "Informations d'identification erronnées!"));
+            // si c'est ok on crée un token
+           
+            const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+            // enlever le mdp
+            const {password: pass, ...rest} = validUser._doc;
+            // création de cookie
+            res.cookie('acces_token', token, {
+
+                httpOnly:true
+            }).status(200).json(rest)
+
+
+        } catch (error) {
+            next(error)
+        }
+    }
